c4964ec6ff396666d509efbe8c487e716b7c2fae
Enrico Matrullo <80920594+HenryMatru@users.noreply.github.com>
2022-12-21 17:46:44 +0100

    Add files via upload

---
+++ ServletExample/src/main/java/it/course/servletexample/NameServlet.java
@@ -0,0 +1,26 @@
+package it.course.servletexample;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "NameServlet", value = "/NameServlet")
+public class NameServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String name = "Enrico";
+        String newPage = "name.jsp";
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(newPage);
+        request.setAttribute("name", name);
+
+        requestDispatcher.forward(request, response);
+    }
+
+}
