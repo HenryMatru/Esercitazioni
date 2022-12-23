05d8f48aba479ac518b7441ad957b88246265e8b
Enrico Matrullo <80920594+HenryMatru@users.noreply.github.com>
2022-12-23 22:58:30 +0100

    Add files via upload

---
+++ ServletUserEsempio2/src/main/java/it/course/servletuseresempio2/controllers/ServletUpdateUser.java
@@ -0,0 +1,54 @@
+package it.course.servletuseresempio2.controllers;
+
+import it.course.servletuseresempio2.dao.UserDAO;
+import it.course.servletuseresempio2.models.User;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "ServletUpdateUser", value = "/ServletUpdateUser")
+public class ServletUpdateUser extends HttpServlet {
+
+    private UserDAO userDAO;
+
+    public void init() {
+        userDAO = new UserDAO();
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        System.out.println(id);
+        User user = userDAO.selectUser(id);
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("update_user.jsp");
+        request.setAttribute("user", user);
+
+        requestDispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String country = request.getParameter("country");
+        int age = Integer.parseInt(request.getParameter("age"));
+        User newUser = new User(id, name, email, country, age);
+        try {
+            userDAO.updateUser(newUser);
+        } catch (SQLException e) {
+            // throw new RuntimeException(e);
+            requestDispatcher = request.getRequestDispatcher("error.jsp");
+            request.setAttribute("errore", e.getMessage());
+        }
+        requestDispatcher.forward(request, response);
+    }
+
+}
