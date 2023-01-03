46242af5d38527b18b802382cd746cbf778fc708
Enrico Matrullo <80920594+HenryMatru@users.noreply.github.com>
2023-01-03 15:23:19 +0100

    Add files via upload

---
+++ EsercitazioneSpringBoot_CourseDatabase/src/main/java/it/course/esercitazionespringbootcourse/EsercitazioneSpringBootCourseApplication.java
@@ -0,0 +1,13 @@
+package it.course.esercitazionespringbootcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercitazioneSpringBootCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsercitazioneSpringBootCourseApplication.class, args);
+    }
+
+}
