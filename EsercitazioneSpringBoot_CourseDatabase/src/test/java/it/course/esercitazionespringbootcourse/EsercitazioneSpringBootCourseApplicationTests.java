e788fbb43095f54af4fcf2d1059d960f792b2115
Enrico Matrullo <80920594+HenryMatru@users.noreply.github.com>
2023-01-03 16:20:34 +0100

    Add files via upload

---
+++ EsercitazioneSpringBoot_CourseDatabase/src/test/java/it/course/esercitazionespringbootcourse/EsercitazioneSpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.course.esercitazionespringbootcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercitazioneSpringBootCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
