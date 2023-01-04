4b3e97a32cea5d8e0627a5ee48b3e8892010f18f
Enrico Matrullo <80920594+HenryMatru@users.noreply.github.com>
2023-01-04 11:00:46 +0100

    Delete EsercitazioneSpringBoot_CoursesDatabase directory

--- EsercitazioneSpringBoot_CoursesDatabase/src/main/java/it/course/esercitazionespringboot_coursesdatabase/repositories/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package it.course.esercitazionespringboot_coursesdatabase.repositories;
-
-import it.course.esercitazionespringboot_coursesdatabase.models.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
