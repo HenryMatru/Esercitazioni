8c149af856b45096ce18ad402ca3353c18f5392c
Enrico Matrullo <80920594+HenryMatru@users.noreply.github.com>
2023-01-04 16:05:21 +0100

    Esercitazione Spring Boot Courses Database

---
+++ EsercitazioneSpringBoot_CoursesDatabase/src/test/java/it/course/esercitazionespringboot_coursesdatabase/EsercitazioneSpringBootCoursesDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.course.esercitazionespringboot_coursesdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercitazioneSpringBootCoursesDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
