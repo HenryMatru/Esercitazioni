81fcdf8386bac8b186b217d2e8334241343c4b41
Enrico Matrullo <80920594+HenryMatru@users.noreply.github.com>
2023-01-16 09:43:30 +0100

    Delete SpringJsp directory

--- SpringJsp/src/main/java/it/course/springjsp/business/impl/DetailBOImpl.java
+++
@@ -1,9 +0,0 @@
-package it.course.springjsp.business.impl;
-
-import it.course.springjsp.business.interfaces.DetailsBO;
-import org.springframework.stereotype.Service;
-
-@Service
-public class DetailBOImpl implements DetailsBO {
-
-}
